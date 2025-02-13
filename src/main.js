641a120ae3428ae908188aa3621da616d400358f
Sara <sarailic431@gmail.com>
2025-02-13 20:28:24 +0100

    challenge-1

--- src/main.js
+++ src/main.js
@@ -1,52 +1,77 @@
 "use strict";
 import "./style.css";
 
-const Person = function (firstName, birthYear) {
-  // Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   // Instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // Never create method inside of constructor function
-  // this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
+//   // Never create method inside of constructor function
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
+
+// const jonas = new Person("Jonas", 1991);
+// console.log(jonas);
+
+// // 1. New {} is created
+// // 2. function is called, this = {}
+// // 3. {} is linked to prototype
+// // 4. function automatically return {}
+
+// const matilda = new Person("Matilda", 2017);
+// const jack = new Person("Jack", 1975);
+// console.log(matilda, jack);
+
+// console.log(jonas instanceof Person);
+
+// // Prototypes
+
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// jonas.calcAge();
+// matilda.calcAge();
+
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// // .prototypeOfLinkedObjects
+
+// Person.prototype.species = "Homo Sapiens";
+// console.log(jonas.species, matilda.species);
+
+// console.log(jonas.hasOwnProperty("firstName"));
+// console.log(jonas.hasOwnProperty("species"));
+
+// Challenge 1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const jonas = new Person("Jonas", 1991);
-console.log(jonas);
-
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} is linked to prototype
-// 4. function automatically return {}
-
-const matilda = new Person("Matilda", 2017);
-const jack = new Person("Jack", 1975);
-console.log(matilda, jack);
-
-console.log(jonas instanceof Person);
-
-// Prototypes
-
-console.log(Person.prototype);
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-jonas.calcAge();
-matilda.calcAge();
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-// .prototypeOfLinkedObjects
-
-Person.prototype.species = "Homo Sapiens";
-console.log(jonas.species, matilda.species);
-
-console.log(jonas.hasOwnProperty("firstName"));
-console.log(jonas.hasOwnProperty("species"));
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
